b59776d3d6339749289c41950e63d570e6d6853b
Elizaveta Lvova-Kirisik <e.kirisik@yandex.ru>
2022-08-08 17:59:40 +0300

    Done Ex_04_Random + find in Random

--- Example004_Random/Program.cs
+++ Example004_Random/Program.cs
@@ -1,10 +1,14 @@
-﻿void NewArray(int[] RundomArray)
+﻿bool Done = false;
+Console.WriteLine("Введите число, которое надо найти:");
+        int find = Convert.ToInt32(Console.ReadLine());
+
+void NewArray(int[] RundomArray)
 {
     int length = RundomArray.Length;
     int index = 0;
     while (index < length)
     {
-        RundomArray[index] = new Random().Next(1, 10);
+        RundomArray[index] = new Random().Next(1, 5);
         index++;
     }
 }
@@ -20,6 +24,30 @@ void PrintArray(int[] Printed)
     }
 }
 
+
+int IndexOf (int[] RundomArray, int find)
+{
+    int l = RundomArray.Length;
+    int i = 0;
+    int position = 0;
+    while ( i < l)
+    {
+        if (RundomArray[i] == find)
+        {
+            Done = true;
+            position = i;
+            Console.WriteLine($"Это число является {position + 1} элементом массива");
+        }
+        i++;
+    }
+    return position;
+}
+
+
 int[] array = new int[10];
 NewArray(array);
 PrintArray(array);
+Console.WriteLine();
+
+int result = IndexOf(array, find);
+Console.WriteLine(Done ? "Больше это число не встречается в массиве" : "Такого числа нет в массиве.");
