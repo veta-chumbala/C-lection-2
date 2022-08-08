12b11dcca63e108dd29990520ee64757204d4057
Elizaveta Lvova-Kirisik <e.kirisik@yandex.ru>
2022-08-08 17:07:15 +0300

    Done Ex_04_Random

---
+++ Example004_Random/Program.cs
@@ -0,0 +1,25 @@
+﻿void NewArray(int[] RundomArray)
+{
+    int length = RundomArray.Length;
+    int index = 0;
+    while (index < length)
+    {
+        RundomArray[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] Printed)
+{
+    int count = Printed.Length;
+    int place = 0;
+    while (place < count)
+    {
+        Console.Write($"{Printed[place]} ");
+        place++;
+    }
+}
+
+int[] array = new int[10];
+NewArray(array);
+PrintArray(array);
