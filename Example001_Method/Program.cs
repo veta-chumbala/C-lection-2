678b309a8c73ef1b55571808c79091677c1a5aa9
Elizaveta Lvova-Kirisik <e.kirisik@yandex.ru>
2022-08-08 13:15:53 +0300

    Done Ex001_Method

--- Example001_Method/Program.cs
+++ Example001_Method/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Max (int arg1, int arg2, int arg3)
+// {
+//     int result = arg1;
+//     if (arg1 < arg2) result = arg2;
+//     if (arg2 < arg3) result = arg3;
+//     return result;
+// }
+
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int a1 = 15;
+int b1 = 21;
+int c1 = 3956;
+int a2 = 185;
+int b2 = 23;
+int c2 = 354323;
+int a3 = 13; 
+int b3 = 291;
+int c3 = 31;
+
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+// int max = Max(max1, max2, max3);
+
+int max = Max(
+    Max(a1, b1, c1),
+    Max(a2, b2, c2),
+    Max(a3, b3, c3)
+);
+
+Console.Write (max);
