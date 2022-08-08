a37a4a9a9e980c3686b0bdd9614dbc5d2060ff3a
Elizaveta Lvova-Kirisik <e.kirisik@yandex.ru>
2022-08-08 16:12:28 +0300

    Done find

---
+++ Example003_find/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] array = { 29, 402, 64, 74, 25, 78, 25, 402, 84 };
+        bool Done = false;
+        int n = array.Length;
+        Console.WriteLine("Введите число, которое надо найти:");
+        int find = Convert.ToInt32(Console.ReadLine());
+        int index = 0;
+
+
+        while (index < n)
+        {
+            if (array[index] == find)
+            {
+                Done = true;
+                break;
+            }
+            index += 1;
+        }
+        Console.WriteLine(Done ? $"Это число является {index + 1} элементом массива" : "Такого числа нет в массиве.");
+    }
+}
